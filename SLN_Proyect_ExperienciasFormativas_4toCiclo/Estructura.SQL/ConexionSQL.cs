5296e40485d22af58d5a1a24b5d077e7990b908f
LuisRuiz4200 <lr27072001@gmail.com>
2023-03-12 16:16:56 -0500

    OK

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Estructura.SQL/ConexionSQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using Proyect_ExperienciasFormativas_4toCiclo;
+
+namespace Estructura.SQL
+{
+    public class ConexionSQL
+    {
+        public String CAD_CN = ConfigurationManager.ConnectionStrings["CN1"].ConnectionString;
+    }
+}
