0c13bdee11a2e326b8037273d944084e52a1a1b1
LuisRuiz4200 <lr27072001@gmail.com>
2023-03-12 19:19:23 -0500

    MAGIÑA GAY

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Dominio.Entidad/PersonaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidad
+{
+    public class PersonaModel
+    {
+        public string idPersona {set;get;}
+        public string namePersona { set;get;}
+        public string descPersona { set;get;}   
+    }
+}
