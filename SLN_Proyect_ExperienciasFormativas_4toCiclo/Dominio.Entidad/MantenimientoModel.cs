d4852123e9603192ced9bc669515ed286f2e2501
LuisRuiz4200 <lr27072001@gmail.com>
2023-03-19 03:07:18 -0500

    OK

--- SLN_Proyect_ExperienciasFormativas_4toCiclo/Dominio.Entidad/MantenimientoModel.cs
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Dominio.Entidad/MantenimientoModel.cs
@@ -8,12 +8,13 @@ namespace Dominio.Entidad
     public class MantenimientoModel
     {
         public string id_mante { set; get; }
-        public string cod_patrimonial { set; get; } 
+        public string cod_patrimonial { set; get; }
         public string cod_tecnico { set; get; }
         public int cod_tipoMante { set; get; }
         public string obs_tipoMante { set; get; }
         public string estado_equipo { set; get; }
-        public DateTime fecha_mante { set; get; }   
-        public string estado_mante { set; get; }
+        public DateTime fecha_mante { set; get; }
+        public string estado_mante { set; get; } 
+        //fin del modelo
     }
 }
