9c3bf7689f0e3c53b1203eaa08680ca46cd08a45
LuisRuiz4200 <lr27072001@gmail.com>
2023-03-19 03:33:44 -0500

    REFERENCIAS Y PRIMER CONTROLADOR

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Proyect_ExperienciasFormativas_4toCiclo/Controllers/DetEquipoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dominio.Entidad;
+
+namespace Proyect_ExperienciasFormativas_4toCiclo.Controllers
+{
+    public class DetEquipoController : Controller
+    {
+        // GET: DetEquipo
+        public ActionResult listarDetEquipo()
+        {
+            return View();
+        }
+        public ActionResult registrarDetEquipo(DetEquipoModel obj) 
+        { 
+            return View(); 
+        
+        }
+        [HttpPost]
+        public ActionResult registrarDetEquipo()
+        {
+            return View();
+
+        }
+    }
+}
