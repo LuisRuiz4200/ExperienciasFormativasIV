d8a135ebdacf1a402e414fd19ea173e5d94261bd
JeanPierre23cpu <jean23.32naej@gmail.com>
2023-03-22 22:59:18 -0500

    UsuarioController
    Al grupo

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Proyect_ExperienciasFormativas_4toCiclo/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace Proyect_ExperienciasFormativas_4toCiclo.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        UsuarioBL usuBL = new UsuarioBL();  
+
+        // GET: Usuario
+        public ActionResult ListarUsuario()
+        {
+            var listado = usuBL.Listar_Usuario();
+
+            return View(listado);
+        }
+    }
+}
