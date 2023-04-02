55e77397371b376a8e52824105df2b95d0909648
Ricardo <Ricardo@Ragnarok>
2023-04-02 09:16:19 -0500

    Implementación Mantenimiento
    Model, DAO, BL, Controller, View

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Dominio.Negocio/MantenimientoBL.cs
@@ -0,0 +1,29 @@
+﻿using Dominio.Entidad;
+using Estructura.SQL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Negocio
+{
+    public class MantenimientoBL
+    {
+        MantenimientoDAO dao = new MantenimientoDAO();
+
+        public List<MantenimientoModel> PA_LISTAR_MANTENIMIENTO()
+        {
+            var listado = dao.PA_LISTAR_MANTENIMIENTO();
+            return listado;
+
+        }
+
+        public string PA_INSERTAR_MANTENIMIENTO(MantenimientoModel obj)
+        {
+            var mensaje = dao.PA_INSERTAR_MANTENIMIENTO(obj);
+            return mensaje;
+        }
+    }
+}
---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Proyect_ExperienciasFormativas_4toCiclo/Controllers/MantenimientoController.cs
@@ -0,0 +1,50 @@
+﻿using Dominio.Entidad;
+using Dominio.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyect_ExperienciasFormativas_4toCiclo.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        MantenimientoBL mantBL = new MantenimientoBL();
+
+        // GET: Mantenimiento
+        public ActionResult listarMantenimientos()
+        {
+            var listado = mantBL.PA_LISTAR_MANTENIMIENTO();
+
+            return View(listado);
+        }
+
+        public ActionResult registrarMantenimiento()
+        {
+            MantenimientoModel model = new MantenimientoModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult registrarMantenimiento(MantenimientoModel obj)
+        {
+            string men = "INGRESE DATOS EN LOS CUADROS";
+
+            try
+            {
+                men = mantBL.PA_INSERTAR_MANTENIMIENTO(obj);
+            }
+            catch (SqlException ex)
+            {
+
+                men = ex.Message;
+            }
+
+            ViewBag.MENSAJE = men;
+
+            return View(obj);
+        }
+    }
+}
