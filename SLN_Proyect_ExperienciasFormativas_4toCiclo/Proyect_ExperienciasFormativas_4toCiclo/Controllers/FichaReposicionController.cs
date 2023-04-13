2909b87837a9ba3069b8608c6b6db40cda39b7fb
JeanPierre23cpu <jean23.32naej@gmail.com>
2023-04-13 01:06:45 -0500

    REPOSICION

---
+++ SLN_Proyect_ExperienciasFormativas_4toCiclo/Proyect_ExperienciasFormativas_4toCiclo/Controllers/FichaReposicionController.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Entidad;
+using Dominio.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyect_ExperienciasFormativas_4toCiclo.Controllers
+{
+    public class FichaReposicionController : Controller
+    {
+        FichaReposicionBL fichRepBL = new FichaReposicionBL();
+
+        // GET: FichaReposicion
+        public ActionResult listarFichaRepuesto()
+        {
+            var lista = fichRepBL.PA_LISTAR_FICHAREPOSICION();
+            return View(lista);
+        }
+    }
+}
